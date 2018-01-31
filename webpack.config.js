11c9a1afc86e6c4a21e7746468d12bda7e1fc45c
Halú de Cássia <haluanedecassia@gmail.com>
2018-01-31 18:07:25 -0200

    Commit inicial

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+    entry: './src/App.js',
+    output: {
+        path: __dirname + '/public',
+        filename: './app.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
+        }
+    },
+    plugins: [
+        extractSass
+    ],
+    module: {
+        loaders: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }, {
+            test: /\.[s]?css$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+            loader: 'file-loader'
+        }]
+    }
+}
